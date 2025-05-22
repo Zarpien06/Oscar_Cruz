9c4a9942e6b661e545911fda39cc63d5d66032e9
Oscar Cruz <oscarcruzsena2006@gmail.com>
2025-05-22 13:03:25 -0500

    Login.tsx
    Se añadio el nav y el footer

--- Proyecto/Frontend/src/Pages/Login.tsx
+++ Proyecto/Frontend/src/Pages/Login.tsx
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom'; // Si usas react-router
 import '../assets/css/FormAuth.css';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
 
 const Login: React.FC = () => {
     const [email, setEmail] = useState('');
@@ -22,36 +25,40 @@ const Login: React.FC = () => {
     };
 
     return (
-        <div className="form-container">
-            <h2>Iniciar Sesión</h2>
-            <form onSubmit={handleLogin}>
-                <input
-                    type="email"
-                    placeholder="Correo electrónico"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                />
-                <input
-                    type="password"
-                    placeholder="Contraseña"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                />
-                <button type="submit">Entrar</button>
-            </form>
+        <>
+        <Navbar scrolled={false} />
+            <div className="form-container">
+                <h2>Iniciar Sesión</h2>
+                <form onSubmit={handleLogin}>
+                    <input
+                        type="email"
+                        placeholder="Correo electrónico"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                    <input
+                        type="password"
+                        placeholder="Contraseña"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                    <button type="submit">Entrar</button>
+                </form>
 
-            <div className="toggle-auth">
-                <p>
-                    ¿No tienes una cuenta?{' '}
-                    <button type="button" onClick={() => navigate('/registro')}>
-                        Regístrate
-                    </button>
-                </p>
+                <div className="toggle-auth">
+                    <p>
+                        ¿No tienes una cuenta?{' '}
+                        <button type="button" onClick={() => navigate('/registro')}>
+                            Regístrate
+                        </button>
+                    </p>
+                </div>
             </div>
-        </div>
-    );
-};
+            <Footer />
+        </>
+    )
+}
 
 export default Login;
