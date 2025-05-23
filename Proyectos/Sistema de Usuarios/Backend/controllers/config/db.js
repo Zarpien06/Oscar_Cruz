a5f38ded43e7d1ce78e7de5905976d369b33b98c
Oscar Cruz <oscarcruzsena2006@gmail.com>
2025-05-23 11:25:43 -0500

    db.js

--- Proyectos/Sistema de Usuarios/Backend/controllers/config/db.js
+++ Proyectos/Sistema de Usuarios/Backend/controllers/config/db.js
@@ -2,15 +2,15 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 db.connect(err => {
-  if (err) throw err;
-  console.log("Base de datos conectada");
+    if (err) throw err;
+    console.log("📦 Conectado a la base de datos MySQL");
 });
 
 module.exports = db;
