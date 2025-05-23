72d04cf55b13d6adae0793f84313369aa15257c2
Oscar Cruz <oscarcruzsena2006@gmail.com>
2025-05-23 09:41:02 -0500

    init.sql

---
+++ Proyectos/Sistema de Usuarios/Sql/init.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS authdb;
+USE authdb;
+
+CREATE TABLE usuarios (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nombre VARCHAR(100),
+  email VARCHAR(100) UNIQUE,
+  password VARCHAR(255)
+);
